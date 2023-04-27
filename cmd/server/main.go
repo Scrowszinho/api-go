5f7083c9287f5a5addd434949dcd33a34c0909f8
Gustavo <gustascrows@gmail.com>
2023-04-26 21:09:36 -0300

    Implementando middlewares

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -29,6 +29,7 @@ func main() {
 	productHandler := handlers.NewProductHandler(productDB)
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
 	r.Route("/products", func(r chi.Router) {
 		r.Use(jwtauth.Verifier(config.TokenAuth))
